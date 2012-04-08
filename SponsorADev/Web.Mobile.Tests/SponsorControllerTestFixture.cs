e87a09a8ed06270e5ca010f3aaa16d6002985b2e
Sam Jeffress <samjeffress@gmail.com>
2012-04-08 14:03:47 +1000

    setup initial web project with test project

---
+++ SponsorADev/Web.Mobile.Tests/SponsorControllerTestFixture.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Web.Mobile.Tests
+{
+    [TestFixture]
+    public class SponsorControllerTestFixture
+    {
+        [Test]
+        public void LoggedInUserWithSponsorship()
+        {
+            //var controller = new SponsorController();
+            //var result = controller.Index();
+
+        }
+
+        [Test]
+        public void LoggedInUserNoSponsorship()
+        {
+            
+        }
+
+        [Test]
+        public void NotLoggedIn()
+        {
+            
+        }
+    }
+}
---
+++ SponsorADev/Web.Mobile/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Mobile.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to kick-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your quintessential app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your quintessential contact page.";
+
+            return View();
+        }
+    }
+}
